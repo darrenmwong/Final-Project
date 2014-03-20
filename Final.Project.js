2c4930434a5728f653a8171eea09b02da4b0216f
darrenwong916 <darrenwong06@gmail.com>
2014-03-20 12:26:24 -0700

    geolocation function added

--- Final.Project.js
+++ Final.Project.js
@@ -1,15 +1,41 @@
 if (Meteor.isClient) {
 
   Template.mapPost.rendered = function() {
-    var mapOptions = {
-        center: new google.maps.LatLng(37.7645, -122.4294),
-        zoom: 10
-    }
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
-    };
+    if (navigator.geolocation) { //Checks if browser supports geolocation
+        navigator.geolocation.getCurrentPosition(function (position) {                                                              //This gets the
+     var latitude = position.coords.latitude;                    //users current
+     var longitude = position.coords.longitude;                 //location
+     var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
+   //  var directionsService = new google.maps.DirectionsService();
+  //   var directionsDisplay = new google.maps.DirectionsRenderer();
+     var mapOptions = //Sets map options
+     {
+       zoom: 15,  //Sets zoom level (0-21)
+       center: coords, //zoom in on users location
+       mapTypeControl: true, //allows you to select map type eg. map or satellite
+       navigationControlOptions:
+       {
+         style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
+       },
+       mapTypeId: google.maps.MapTypeId.ROADMAP //sets type of map Options:ROADMAP, SATELLITE, HYBRID, TERRIAN
+     };
+     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    // directionsDisplay.setMap(map);
+    // directionsDisplay.setPanel(document.getElementById('panel'));
+    // var request = {
+    //   origin: coords,
+     //  destination: 'BT42 1FL',
+     //  travelMode: google.maps.DirectionsTravelMode.DRIVING
+    // };
 
-  google.maps.event.addDomListener(window, 'load', initialize);  
+    // directionsService.route(request, function (response, status) {
+     //  if (status == google.maps.DirectionsStatus.OK) {
+      //   directionsDisplay.setDirections(response);
+      // }
+    // });
+   });
+ }
+}; 
 
   Template.hello.greeting = function () {
     return "Welcome to Final.Project.";
@@ -17,7 +43,6 @@ if (Meteor.isClient) {
 
   Template.hello.events({
     'click input': function () {
-      // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     }
