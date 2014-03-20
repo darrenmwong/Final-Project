ac99aec813af211878365adb6c1dea55d7e4e6b9
darrenwong916 <darrenwong06@gmail.com>
2014-03-20 09:58:26 -0700

    initialized project in meteor

---
+++ Final.Project.js
@@ -0,0 +1,29 @@
+if (Meteor.isClient) {
+  Template.mapPost.rendered = function() {
+    var mapOptions = {
+        center: new google.maps.LatLng(37.7645, -122.4294),
+        zoom: 10
+    };
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+};
+    
+
+  Template.hello.greeting = function () {
+    return "Welcome to Final.Project.";
+  };
+
+  Template.hello.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
