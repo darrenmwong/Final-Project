a838bd4e906100b290ea365666dd4c0820114792
darrenwong916 <darrenwong06@gmail.com>
2014-03-20 10:41:10 -0700

    added map functionality

--- Final.Project.js
+++ Final.Project.js
@@ -1,13 +1,15 @@
 if (Meteor.isClient) {
+
   Template.mapPost.rendered = function() {
     var mapOptions = {
         center: new google.maps.LatLng(37.7645, -122.4294),
         zoom: 10
+    }
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
     };
 
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-};
-    
+  google.maps.event.addDomListener(window, 'load', initialize);  
 
   Template.hello.greeting = function () {
     return "Welcome to Final.Project.";
