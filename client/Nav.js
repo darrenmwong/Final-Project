e019c56e6052e15913434d42563cbc2d08080eca
darrenwong916 <darrenwong06@gmail.com>
2014-03-25 16:17:38 -0700

    added profile rendering and form

--- client/Nav.js
+++ client/Nav.js
@@ -7,19 +7,20 @@ if (Meteor.isClient) {
         console.log('WORKING AS INTENDED');
     },
 
-    'click #sign-up' : function() {
-        if (document.getElementById('signup-form')) {
-        $('#signup-form').remove();
-        }
-        
-        else {
-         console.log('render sign-up');
-         $("body").prepend(Template.signup);
-        }
-    },
+//    'click #sign-up' : function() {
+//        if (document.getElementById('signup-form')) {
+//        $('#signup-form').remove();
+//        }
+//        
+//        else {
+//         console.log('render sign-up');
+//         $("body").prepend(Template.signup);
+//        }
+//    },
 
     'click #profile' : function() {
         if (document.getElementById("profile-page")) { 
+
             $('#profile-page').remove(); 
             console.log('deleted dom element profile-page');
            }     
--- client/signup.js
+++ client/signup.js
@@ -4,7 +4,7 @@ if (Meteor.isClient) {
       e.preventDefault();
       var email = t.find('#account-email').value
         , password = t.find('#account-password').value;
-
+          
         // Trim and validate the input
 
       Accounts.createUser({email: email, password : password}, function(err){
