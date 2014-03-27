3f0fe6aa5ad03503cd010e2179b12731c1ee09d4
darrenwong916 <darrenwong06@gmail.com>
2014-03-27 11:21:40 -0700

    can add all users in area to an array

--- client/Nav.js
+++ client/Nav.js
@@ -1,20 +1,48 @@
 if (Meteor.isClient) {
 
- alertSubmit = function() {
+    var getDistanceBetween = function(lat1, lon1, lat2, lon2) {
+        var R = 6371; //Radius of earth in km
+        var dLat = deg2rad(lat2-lat1); 
+        var dLon = deg2rad(lon2-lon1);
+        var a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; // Distance in km
+             return d;
+    };
+    
+    var deg2rad = function(deg) {
+        return deg * (Math.PI/180);
+    };
+
+    
+  alertSubmit = function() {
     var UsersInR = [];
+    var distance = 0.402336; //.25 miles in KM
     var des = $('#alertDescription').val();
     console.log(des);
+// finding user profile and getting the longitude latitude of the user
     var woot = Profile.findOne({id: Meteor.userId()});
-        console.log(woot.coords);
+    var centerlng = woot.coords.k;
+    var centerlat = woot.coords.A;
+// finding ALL users and putting them into an array using fetch
     var profile = Profile.find();
     var fetch = profile.fetch();
         console.log(fetch);
-//        if(navigator.geolocation) {
-//            navigator.geolocation.getCurrentPosition(function(position) {
-//            console.log('geolocation is on');
-//        var center = position.coords;  
-//            console.log(point);
-//            });
+// Looping through all profiles
+    for(var i=0; i < fetch.length; i++) {
+         var lng = fetch[i].coords.k;
+         var lat = fetch[i].coords.A;
+         var between = getDistanceBetween(centerlat, centerlng, lat, lng);
+// Checks if distance between is less then .25 mile radius
+         if(between <= distance) {
+            UsersInR.push(fetch[i].id);
+        }; 
+    }
+        console.log(UsersInR);
     
         
              
