8da11a2b9f1d878abcc9031081b43ca845c5a6b3
darrenwong916 <darrenwong06@gmail.com>
2014-03-28 14:59:38 -0700

    deleted files

--- Nav.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.isClient) {
-Template.menu.events({
-    'click .menu-toggle' : function() {
-        console.log('clicked toggle menu');
-        $('.menu-border'._id).slideUp('slow');
-        console.log('WORKING AS INTENDED');
-    }
-});
-};
